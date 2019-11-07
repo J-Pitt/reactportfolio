78ce475aa28170e7d10af5318236eb15e3811721
Jason Pittman <jpittman@Jasons-MacBook-Pro.local>
2019-11-07 21:51:31 +0000

    adding react portfolio

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section id="about">
+        <div>
+          <img className="profile-pic" src="https://media.licdn.com/dms/image/C4E03AQF1k9tMSdqqCA/profile-displayphoto-shrink_200_200/0?e=1578528000&v=beta&t=QzIe_cspU_8jYI3WwYiOsg00PN_zEPeNRiUmW41O5ME" alt="" />
+          <p>I'm Jason</p>
+        </div>
+    </section>
+  );
+}
+
+
+export default About;
